5e32e288014d69b98e49ec1f0e24bb2ed89dcdfb
DryBloodRaven <amproctor@gmail.com>
2020-10-10 21:52:40 -0700

    having looping issues

--- Develop/script.js
+++ Develop/script.js
@@ -14,8 +14,7 @@ function writePassword() {
 }
 
 
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
+
 // WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
 // WHEN all prompts are answered
@@ -30,36 +29,67 @@ var numberChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 function generatePassword() {
+  debugger;
   // Generate password length. need to validate that it is between 8 and 128
   passwordLength = window.prompt("How many characters would you like you password to contain?", "8");
-  // Validate response
+
+  // Validate response is between 8 and 128 characters
   if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Invalid character length. Please choose a number between 8 and 128. Thank you.");
-    return generatePassword();
+    return generatePassword(); //return to start if invalid response is given
   }
+  // Validate that response is a valid number response
   if (Number.isNaN(passwordLength)) {
     window.alert("Invalid format. Please choose a number between 8 and 128. Thank you.");
-    return generatePassword();
+    return generatePassword(); //return to start if invalid response is given
   }
 
-  // Select character parameters
+  //creating a new array for when we combine the above (lowerChar, upperChar, numberChar and specialChar) arrays based on the following responses
+  var combinedChar =[]
+
+  // Select character parameters (special, number, lowercase and uppercase)
   var specialConfirm = window.confirm("Click OK to confirm including special characters.");
+  // If above confirm is true than the follow if statement will add the specialChar array into the combinedChar array
+  if (specialConfirm) {
+    combinedChar = combinedChar.concat(specialChar);
+  };
 
   var numberConfirm = window.confirm("Click OK to confirm including numeric characters.");
+  // If above confirm is true than the follow if statement will add the numberChar array into the combinedChar array
+  if (numberConfirm) {
+    combinedChar = combinedChar.concat(numberChar);
+  };
 
   var lowerConfirm = window.confirm("Click OK to confirm including lowercase characters.");
+  // If above confirm is true than the follow if statement will add the lowerChar array into the combinedChar array
+  if (lowerConfirm) {
+    combinedChar = combinedChar.concat(lowerChar);
+  };
 
   var upperConfirm = window.confirm("Click OK to confirm including uppercase characters.");
+  // If above confirm is true than the follow if statement will add the upperChar array into the combinedChar array
+  if (upperConfirm) {
+    combinedChar = combinedChar.concat(upperChar);
+  };
 
   // Validate that at least 1 character parameter was selected
   if (!specialConfirm && !numberConfirm && !lowerConfirm && !upperConfirm) {
     window.alert("Invlaid selection. You must include at least 1 character type");
-    return generatePassword();
-  }
+    return generatePassword(); //return to start if invalid response is given
+  };
+
+  // Created new variable for the password that will be generated
+  var generatedPassword = "";
+
+  // TOSO: Getting stuck in a loop and not stopping at the correct password length
+  for (var i = 0; i = passwordLength; i++) {
+    generatedPassword = generatedPassword + combinedChar[Math.floor(Math.random() * combinedChar.length)];
+    console.log(generatePassword);
+  };
+
+  return generatedPassword;
+
 }
-
-
-generatePassword();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
